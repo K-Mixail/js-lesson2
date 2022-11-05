b800c7938cee77496d9bbfb318722ca6dd9cc85e
K-Mixail <131530@mail.ru>
2022-11-05 14:52:13 +0300

    тернарный оператор

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-let age = 12;
-const gender = 'муж'; // 'жен'
+// let age = 12;
+// const gender = 'муж'; // 'жен'
 
-age = +age;
+// age = +age;
 
-if (!isNaN(age)) {
-    if (age > 18) {
-        console.log("Вам больше 18 лет");
-    } else if (age < 18) {
-        console.log("Вам меньше 18 лет");
-    } else if (age === 18) {
-        console.log("Вам ровно 18 лет");
-    } else {
-        console.log("Error");
-    }
-} else {
-    console.log("введите число")
-}
+// if (!isNaN(age)) {
+//     if (age > 18) {
+//         console.log("Вам больше 18 лет");
+//     } else if (age < 18) {
+//         console.log("Вам меньше 18 лет");
+//     } else if (age === 18) {
+//         console.log("Вам ровно 18 лет");
+//     } else {
+//         console.log("Error");
+//     }
+// } else {
+//     console.log("введите число")
+// }
 
 
-if (gender.toLocaleLowerCase() === 'муж') {
-    console.log("поверните налево");
-}
+// if (gender.toLocaleLowerCase() === 'муж') {
+//     console.log("поверните налево");
+// }
 
-if (gender.toLocaleLowerCase() === 'жен') {
-    console.log("поверните направо");
-}
+// if (gender.toLocaleLowerCase() === 'жен') {
+//     console.log("поверните направо");
+// }
+
+
+// const isAdmin = true;
+// console.log(`Добро пожаловать: ${isAdmin ? "Администратор" : 'Пользователь'}`);
