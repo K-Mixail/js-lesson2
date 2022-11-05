aff9bd04b72df240081b58ee817b653fe79e0207
K-Mixail <131530@mail.ru>
2022-11-05 15:20:04 +0300

    switc-case

--- app.js
+++ app.js
@@ -29,3 +29,22 @@
 
 // const isAdmin = true;
 // console.log(`Добро пожаловать: ${isAdmin ? "Администратор" : 'Пользователь'}`);
+
+const typeUser = 'bost'; // user, guest, bot
+switch(typeUser) {
+    case 'admin':
+        console.log('Вы администратор');
+        break;
+    case 'user':
+        console.log('Вы пользователь');
+        break;
+    case 'guest':
+        console.log('Вы гость');
+        break;
+    case 'bot':
+        console.log('Вы бот');
+        break;
+    default:
+        console.log('Ошибка');
+        break;
+}
