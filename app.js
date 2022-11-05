322dc2ad59cc8d22792d08ea6d7b11e2f430fca6
K-Mixail <131530@mail.ru>
2022-11-05 13:48:52 +0300

    добавление if

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('привет');
+const age = 15;
+const gender = 'муж'; // 'жен'
+
+if (age > 18) {
+    console.log("Вам больше 18 лет");
+}
+
+if (gender.toLocaleLowerCase() === 'муж') {
+    console.log("поверните налево");
+}
+
+if (gender.toLocaleLowerCase() === 'жен') {
+    console.log("поверните направо");
+}
